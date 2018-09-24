379a8070799b0015cb77bd5291f37a0e6705767b
T-Waddell <tara.waddell2@gmail.com>
2018-09-23 20:24:48 -0500

    Created an Index file.

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang='en'>
+<head>
+	<title>Tara Waddell - Project 1</title>
+	<meta charset='utf-8'>
+	
+	<link href='css/main.css' rel='stylesheet'>
+	
+</head>
+<body>
+
+	<h1>Tara Waddell</h1>
+	
+
+</body>
+</html>
