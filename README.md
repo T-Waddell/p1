c72773779f82936465b90045fbb92cab06d7bc62
T-Waddell <tara.waddell2@gmail.com>
2018-09-24 21:46:41 -0500

    Created a CSS file, added a picture, and edited Index.php

--- README.md
+++ README.md
@@ -3,7 +3,9 @@
 + Production URL: <http://p1.yourdomain.com>
 
 ## Outside resources
-*your list of outside resources go here*
+Google fonts:
+* [Amatic](https://fonts.google.com/specimen/Amatic+SC)
+* [Open Sans](https://fonts.google.com/specimen/Open+Sans)
 
 ## Notes for instructor
 *any notes for me to refer to while grading; if none, omit this section*
--- index.php
+++ index.php
@@ -4,13 +4,18 @@
 	<title>Tara Waddell - Project 1</title>
 	<meta charset='utf-8'>
 	
+    <link href="https://fonts.googleapis.com/css?family=Amatic+SC|Open+Sans" rel="stylesheet">
 	<link href='css/main.css' rel='stylesheet'>
 	
 </head>
 <body>
-
+    <div id="wrapper">
 	<h1>Tara Waddell</h1>
+    
+    <img src="images/tara.jpg" alt="Tara Waddell"/>
+    
+    <p>Hi. I'm Tara. I recently earned a Master's Certificate in Front-End Web Development from Harvard Extension School, and I'm working toward a Master's in Digital Media Design. I am excited to learn server-side coding as well as version control and workflow processes. I live in the Great Plains, but I love traveling as much as possible.</p>
 	
-
+    </div>
 </body>
 </html>
